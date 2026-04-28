90c561f13e12141122d3500d6ff03b45484f6287
İshak Bostan <ishakbostan@gmail.com>
2026-04-28 08:31:45 +0300

    Add BookReview model

---
+++ src/Reviews.kt
@@ -0,0 +1,14 @@
+﻿package com.library
+
+data class BookReview(
+    val userId: String,
+    val bookIsbn: String,
+    val rating: Int,
+    val comment: String
+) {
+    init {
+        require(rating in 1..5) { "Rating must be between 1 and 5" }
+    }
+    
+    fun display(): String = "Rating: $rating/5 - $comment (by $userId)"
+}
