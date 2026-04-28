788618a9ad452a5097817c4580ea110ddc20214e
İshak Bostan <ishakbostan@gmail.com>
2026-04-28 08:31:39 +0300

    Create LibraryTransaction model

---
+++ src/Transactions.kt
@@ -0,0 +1,19 @@
+﻿package com.library
+
+import java.time.LocalDateTime
+
+enum class TransactionType {
+    BORROW, RETURN
+}
+
+data class LibraryTransaction(
+    val id: String,
+    val userId: String,
+    val bookIsbn: String,
+    val type: TransactionType,
+    val timestamp: LocalDateTime = LocalDateTime.now()
+) {
+    fun summary(): String {
+        return "[$timestamp] $type: User $userId -> Book $bookIsbn"
+    }
+}
