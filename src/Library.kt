3f5fc5d984308f164e5da8046caddff899ec9e37
İshak Bostan <ishakbostan@gmail.com>
2026-04-28 08:31:28 +0300

    Initialize Library class

---
+++ src/Library.kt
@@ -0,0 +1,13 @@
+﻿package com.library
+
+class Library(val name: String) {
+    private val books = mutableMapOf<String, Book>()
+    private val users = mutableMapOf<String, User>()
+
+    fun getBookCount(): Int = books.size
+    fun getUserCount(): Int = users.size
+    
+    fun printStatus() {
+        println("Library $name has $($books.size) books and $($users.size) users.")
+    }
+}
