8ff395215549e206393d868b87705d4958e0e64f
İshak Bostan <ishakbostan@gmail.com>
2026-04-28 08:31:55 +0300

    Initialize Main application

---
+++ src/Main.kt
@@ -0,0 +1,17 @@
+﻿package com.library
+
+fun main() {
+    println("Initializing Library Management System...")
+    val library = Library("Central City Library")
+    
+    val tolkien = Author("a1", "J.R.R. Tolkien", "English writer")
+    val asimov = Author("a2", "Isaac Asimov", "Sci-Fi author")
+    
+    val book1 = Book("isbn1", "The Hobbit", tolkien, Genre.FANTASY)
+    val book2 = Book("isbn2", "Foundation", asimov, Genre.SCIENCE)
+    
+    library.addBook(book1)
+    library.addBook(book2)
+    
+    library.printStatus()
+}
