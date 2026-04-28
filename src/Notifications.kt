f09e06ac9e8db040f3c1264d61ea89b4991849bd
İshak Bostan <ishakbostan@gmail.com>
2026-04-28 08:31:51 +0300

    Add Notification interface and models

---
+++ src/Notifications.kt
@@ -0,0 +1,17 @@
+﻿package com.library
+
+interface NotificationService {
+    fun sendNotification(userId: String, message: String)
+}
+
+class EmailNotificationService : NotificationService {
+    override fun sendNotification(userId: String, message: String) {
+        println("-> [EMAIL to User $userId] $message")
+    }
+}
+
+class SmsNotificationService : NotificationService {
+    override fun sendNotification(userId: String, message: String) {
+        println("-> [SMS to User $userId] $message")
+    }
+}
