44fef9d4461c56c37c659a1bcbd6c09139c7e315
İshak Bostan <ishakbostan@gmail.com>
2026-04-28 08:31:24 +0300

    Add Book model

--- src/Models.kt
+++ src/Models.kt
@@ -11,3 +11,16 @@ data class Author(
 ) {
     override fun toString(): String = name
 }
+
+data class Book(
+    val isbn: String,
+    val title: String,
+    val author: Author,
+    val genre: Genre,
+    var isAvailable: Boolean = true
+) {
+    fun displayInfo(): String {
+        val status = if (isAvailable) "Available" else "Borrowed"
+        return "$title by $($author.name) [$genre] - $status"
+    }
+}
