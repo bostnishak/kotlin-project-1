ef1e56131eb4506ee0a5d6bb3668c6fb835f96e1
İshak Bostan <ishakbostan@gmail.com>
2026-04-29 10:33:08 +0300

    Add truncate extension

--- src/Extensions.kt
+++ src/Extensions.kt
@@ -3,3 +3,8 @@
 fun String.toTitleCase(): String {
     return this.split(" ").joinToString(" ") { it.capitalize() }
 }
+
+fun String.truncate(maxLength: Int): String {
+    if (this.length <= maxLength) return this
+    return this.substring(0, maxLength - 3) + "..."
+}
