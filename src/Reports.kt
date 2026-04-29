f78900f1acb2654d326185d8e2d4ebd45a751e09
İshak Bostan <ishakbostan@gmail.com>
2026-04-29 10:33:00 +0300

    Add inventory report

--- src/Reports.kt
+++ src/Reports.kt
@@ -8,4 +8,8 @@ class Reports {
     fun generateUserReport(userId: String): String {
         return "Report for User $userId"
     }
+
+    fun generateInventoryReport(bookCount: Int): String {
+        return "Total Books in Inventory: $bookCount"
+    }
 }
