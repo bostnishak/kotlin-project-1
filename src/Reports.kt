49d0e7932503ffa11c584aaa8fbcaca629e57673
İshak Bostan <ishakbostan@gmail.com>
2026-04-29 10:32:56 +0300

    Add user report

--- src/Reports.kt
+++ src/Reports.kt
@@ -4,4 +4,8 @@ class Reports {
     fun generateSummary(): String {
         return "Library Summary Report\n-----------------------"
     }
+
+    fun generateUserReport(userId: String): String {
+        return "Report for User $userId"
+    }
 }
