1c3fcda9a9f2e3d7fe73692c1c40fa1ebc94d351
İshak Bostan <ishakbostan@gmail.com>
2026-04-29 10:32:35 +0300

    Add view counting

--- src/Analytics.kt
+++ src/Analytics.kt
@@ -2,8 +2,15 @@
 
 object Analytics {
     private val eventLogs = mutableListOf<String>()
+    private val bookViews = mutableMapOf<String, Int>()
 
     fun logEvent(event: String) {
         eventLogs.add(event)
+    }
+
+    fun logBookView(isbn: String) {
+        val count = bookViews.getOrDefault(isbn, 0)
+        bookViews[isbn] = count + 1
+        logEvent("Viewed book: $isbn")
     }
 }
