8b9bc1d3633b56e7a2c1269436b0acc9aab91075
İshak Bostan <ishakbostan@gmail.com>
2026-05-02 10:56:27 +0300

    Add utility function 14

--- src/Utils.kt
+++ src/Utils.kt
@@ -10,3 +10,4 @@ fun cube(n: Int): Int = n * n * n
 fun greet(name: String): String = "Hello, $name!"
 fun max(a: Int, b: Int): Int = if (a > b) a else b
 fun factorial(n: Int): Int = if (n <= 1) 1 else n * factorial(n - 1)
+fun lengthOfString(s: String): Int = s.length
