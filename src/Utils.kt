effd2522d3ed446c7d61b64adee8167df509e1c0
İshak Bostan <ishakbostan@gmail.com>
2026-05-02 10:56:01 +0300

    Add utility function 7

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,3 +5,4 @@ fun multiply(a: Int, b: Int): Int = a * b
 fun divide(a: Int, b: Int): Double = a.toDouble() / b
 fun isEven(n: Int): Boolean = n % 2 == 0
 fun isOdd(n: Int): Boolean = n % 2 != 0
+fun square(n: Int): Int = n * n
